ffaff9f2e67dfec0c1b3c83041708dddf3ff227d
Rohith2408 <kumarrohith24081999@gmail.com>
2024-07-14 01:05:12 +0530

    update

--- src/components/Howtobuy.tsx
+++ src/components/Howtobuy.tsx
@@ -19,9 +19,9 @@ const Howtobuy: FunctionComponent<HowtobuyType> = ({ className = "" }) => {
         <div className={[styles.title, className].join(" ")}>How To Buy</div>
         <div className={[styles.body, className].join(" ")}>
           <Framecontainer className={className} color="linear-gradient(#FD8A8A, #AD5B5B)" image="/buy1-1@2x.png" title="Download Phantom Extension" subTitle="Download and install ​phantom on the ​appstore, or,​alternatively if ​you’re on desktop ​download and install ​thebrowser ​extension."></Framecontainer>
-          <Framecontainer className={className} color="linear-gradient(#F9B889, #926546)" image="/buy1-1@2x.png" title="Secure your" subTitle="you can buy ​$sol from any ​exchange and ​even on ​phantom ​wallet"></Framecontainer>
-          <Framecontainer className={className} color="linear-gradient(#58A098, #69C6BA)" image="/buy1-1@2x.png" title="Buy some $Marv" subTitle="Now go to ​Raydium and paste ​the $MARV ​Contract Address ​to swap your SOL."></Framecontainer>
-          <Framecontainer className={className} color="linear-gradient(#AC9CDB, #7462A6)" image="/buy1-1@2x.png" title="Add to Your Wallet" subTitle="Now you’re all set! ​Welcome aboard the ​next rocket to the ​moon, just import the ​contract address to ​view you’re holdings ​and you can track ​your earnings."></Framecontainer>
+          <Framecontainer className={className} color="linear-gradient(#F9B889, #926546)" image="/buy_2.png" title="Secure your" subTitle="you can buy ​$sol from any ​exchange and ​even on ​phantom ​wallet"></Framecontainer>
+          <Framecontainer className={className} color="linear-gradient(#58A098, #69C6BA)" image="/buy_3.png" title="Buy some $Marv" subTitle="Now go to ​Raydium and paste ​the $MARV ​Contract Address ​to swap your SOL."></Framecontainer>
+          <Framecontainer className={className} color="linear-gradient(#AC9CDB, #7462A6)" image="/buy_4.png" title="Add to Your Wallet" subTitle="Now you’re all set! ​Welcome aboard the ​next rocket to the ​moon, just import the ​contract address to ​view you’re holdings ​and you can track ​your earnings."></Framecontainer>
         </div>
       </div>
     </section>
