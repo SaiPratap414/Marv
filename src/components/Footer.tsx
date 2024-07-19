32a96c6d6e6d2efe3852d31b61bc7081c1d92e32
Rohith2408 <kumarrohith24081999@gmail.com>
2024-07-19 11:53:22 +0530

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useCallback } from "react";
+import { FunctionComponent, useCallback, useRef } from "react";
 import styles from "./Footer.module.css";
 
 export type FooterType = {
@@ -6,6 +6,15 @@ export type FooterType = {
 };
 
 const Footer: FunctionComponent<FooterType> = ({ className = "" }) => {
+
+  const social=useRef([
+    {title:"TikTok",url:""},
+    {title:"Memes",url:""},
+    {title:"Twitter",url:"https://x.com/Realmarv_Sol"},
+    {title:"Telegram",url:"https://t.me/realmarv_sol"},
+    {title:"Instagram",url:""}
+  ]).current
+
   const onLinkContainerClick = useCallback(() => {
     window.open("https://twitter.com/");
   }, []);
@@ -94,11 +103,11 @@ const Footer: FunctionComponent<FooterType> = ({ className = "" }) => {
               <div className={[styles.gopolatitle, className].join(" ")}>Go Marv!</div>
               <div className={[styles.gopolalist, className].join(" ")}>
                 <div>Join Marvarmy</div>
-                <div>TikTok</div>
-                <div>Memes</div>
-                <div>Twitter</div>
-                <div>Telegram</div>
-                <div>Instagram</div>
+                {
+                  social.map((item)=>
+                  <div><a onClick={()=>window.open(item.url)}>{item.title}</a></div>
+                  )
+                }
               </div>
             </div>
           </div>
@@ -111,195 +120,3 @@ const Footer: FunctionComponent<FooterType> = ({ className = "" }) => {
 export default Footer;
 
 
-//<section className={[styles.footer, className].join(" ")} data-scroll-to="Disclaimer">
-//       <div className={styles.bg}>
-//         <div className={styles.bgChild} />
-//         <img
-//           className={styles.marvWebLastPageWithBg0002}
-//           alt=""
-//           src="/marv-web-last-page-with-bg0002-1@2x.png"
-//         />
-//       </div>
-//       <div className={styles.containerWrapper}>
-//         <div className={styles.container}>
-//           <div className={styles.heading2}>
-//             <div className={styles.disclaimer}>Disclaimer</div>
-//           </div>
-//         </div>
-//       </div>
-//       <div className={styles.footerActions}>
-//         <div className={styles.actionsContainer}>
-//           <form className={styles.footerButtonsWrapper}>
-//             <div className={styles.overlayshadow1}>
-//               <div className={styles.button1}>
-//                 <input
-//                   className={styles.container4}
-//                   placeholder="Risk Disclosure"
-//                   type="text"
-//                 />
-//                 <div className={styles.container5}>
-//                   <div className={styles.container6}>
-//                     <img className={styles.icon1} alt="" src="/icon.svg" />
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//             <div className={styles.overlayshadow2}>
-//               <button className={styles.button2}>
-//                 <div className={styles.link}>
-//                   <div className={styles.buyMarv}>Buy $Marv</div>
-//                 </div>
-//               </button>
-//               <div className={styles.border}>
-//                 <input
-//                   className={styles.margin}
-//                   placeholder="Contract address: "
-//                   type="text"
-//                 />
-//                 <div className={styles.button3}>
-//                   <div className={styles.container7}>
-//                     <img className={styles.icon2} alt="" src="/icon-2.svg" />
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </form>
-//         </div>
-//         <div className={styles.margin1}>
-//           <div className={styles.container8}>
-//             <div className={styles.margin2}>
-//               <div className={styles.container9}>
-//                 <div className={styles.container10}>
-//                   <div className={styles.polasvg}>
-//                     <div className={styles.polaLettersParent}>
-//                       <div className={styles.polaLetters}>
-//                         <div className={styles.mParent}>
-//                           <div className={styles.m}>M</div>
-//                           <div className={styles.m1}>M</div>
-//                         </div>
-//                       </div>
-//                       <div className={styles.aParent}>
-//                         <div className={styles.a}>A</div>
-//                         <div className={styles.a1}>A</div>
-//                       </div>
-//                       <div className={styles.polaLetters1}>
-//                         <div className={styles.rParent}>
-//                           <div className={styles.r}>R</div>
-//                           <div className={styles.r1}>R</div>
-//                         </div>
-//                       </div>
-//                       <div className={styles.vParent}>
-//                         <div className={styles.v}>V</div>
-//                         <div className={styles.v1}>V</div>
-//                       </div>
-//                     </div>
-//                   </div>
-//                   <div className={styles.smallmargin}>
-//                     <div className={styles.small}>
-//                       <div className={styles.joinTheMarv}>
-//                         Join the Marv Army.
-//                       </div>
-//                     </div>
-//                   </div>
-//                   <div className={styles.container11}>
-//                     <div
-//                       className={styles.link1}
-//                       onClick={onLinkContainerClick}
-//                     >
-//                       <div className={styles.container12}>
-//                         <img
-//                           className={styles.icon3}
-//                           loading="lazy"
-//                           alt=""
-//                           src="/tww.png"
-//                         />
-//                       </div>
-//                     </div>
-//                     <div
-//                       className={styles.link2}
-//                       onClick={onLinkContainerClick1}
-//                     >
-//                       <div className={styles.container13}>
-//                         <img
-//                           className={styles.icon4}
-//                           loading="lazy"
-//                           alt=""
-//                           src="/icon-5.svg"
-//                         />
-//                       </div>
-//                     </div>
-//                     <div
-//                       className={styles.link3}
-//                       onClick={onLinkContainerClick2}
-//                     >
-//                       <div className={styles.container14}>
-//                         <img
-//                           className={styles.icon5}
-//                           loading="lazy"
-//                           alt=""
-//                           src="/dex.png"
-//                         />
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//             <div className={styles.margin7}>
-//               <div className={styles.container21}>
-//                 <div className={styles.container22}>
-//                   <div className={styles.goPolaGo}>Go Marv Go!</div>
-//                 </div>
-//                 <div className={styles.container23}>
-//                   <div className={styles.margin8}>
-//                     <div className={styles.container24}>
-//                       <div className={styles.link7}>
-//                         <div className={styles.joinMarvarmy}>TikTok</div>
-//                       </div>
-//                     </div>
-//                   </div>
-//                   <div className={styles.margin9}>
-//                     <div className={styles.container25}>
-//                       <div className={styles.link8}>
-//                         <div className={styles.memes}>Memes</div>
-//                       </div>
-//                     </div>
-//                   </div>
-//                   <div className={styles.margin10}>
-//                     <div className={styles.container26}>
-//                       <div className={styles.link9}>
-//                         <div className={styles.twitter}>Twitter</div>
-//                       </div>
-//                     </div>
-//                   </div>
-//                   <div className={styles.margin11}>
-//                     <div className={styles.container27}>
-//                       <div className={styles.link10}>
-//                         <div className={styles.telegram}>Telegram</div>
-//                       </div>
-//                     </div>
-//                   </div>
-//                   <div className={styles.margin12}>
-//                     <div className={styles.container28}>
-//                       <div className={styles.link11}>
-//                         <div className={styles.reddit}>Instagram</div>
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <div className={styles.homeButtonContainer}>
-//   <img
-//     className={styles.homeButton}
-//     src="/arrow.png"
-//     alt="Home"
-//     onClick={onHomeButtonClick}
-//   />
-//   <div className={styles.homeButtonText}>Punch me to go home</div>
-// </div>
-
-//       </div>
-//     </section>
--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./LoadingScreen.module.css";
 
 const LoadingScreen: React.FC = () => {
+
+  const [layout, setLayout] = useState<"web"|"mobile"|"tab"|"pc"|"laptop">(getLayout());
+
+  useEffect(()=>{
+    window.addEventListener("resize", () => {
+      setLayout(getLayout());
+    });
+  },[])
+
   return (
     <div className={styles.loadingContainer}>
-      <img src="/buy-marv.gif" className={styles.backgroundGif} alt="Loading" />
+      <img src={layout=="mobile"?"/buy-marv-mobile.gif":"/buy-marv.gif"} className={styles.backgroundGif} alt="Loading" />
       <div className={styles.loadingBar}></div>
     </div>
   );
 };
+
+const getLayout = () => {
+  let layout: "web"|"mobile"|"tab"|"pc"|"laptop" = "web";
+  if (window.innerWidth <= 500) {
+    layout = "mobile";
+  }
+  if (window.innerWidth > 500 && window.innerWidth <= 1024) {
+    layout = "tab";
+  }
+  if (window.innerWidth > 1024 && window.innerWidth <= 1920) {
+    layout = "laptop";
+  }
+  if (window.innerWidth > 1920) {
+    layout = "pc";
+  }
+  return layout;
+}
 
 export default LoadingScreen;
